f9dd2f28ee2e3a584ed7e5c61f75ecb737af8690
Alexander Kidanov <a.kidanov@gmail.com>
2023-10-31 21:41:21 +0200

    Added help command + added welcome message in console.  (#4)
    * Welcome message
    
    * get help commands
    
    * update error handler
    
    * added handlers and added usage

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from models.adressbook import AddressBook
 from utils.input_parser import parse_input
+from utils.get_welcome_message import get_welcome_message
+from utils.get_help_commands import get_help_commands
 from utils.handlers import add_contact, change_contact, show_phone, show_all,\
-    add_birthday, show_birthday, birthdays, add_email
+    add_birthday, show_birthday, birthdays, add_email, help
 
 def app():
-    contacts = AddressBook()
+    print(get_welcome_message())
+    print(get_help_commands())
 
-    print("Welcome to the assistant bot!")
+    contacts = AddressBook()
 
     while True:
         user_input = input("Enter a command: ")
@@ -33,5 +36,7 @@ def app():
             print(birthdays(args, contacts))
         elif command == "add-email":
             print(add_email(args, contacts))
+        elif command == "help":
+            print(help(args))
         else:
-            print("Invalid command.")
+            print("Invalid command. Use 'help' to see list of the exist commands ")
