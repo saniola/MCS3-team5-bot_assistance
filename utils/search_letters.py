2cbd20b9a3ac34b6e960569fa09e59b1d3918896
Karpenko Nataliia <48415878+Netaly79@users.noreply.github.com>
2023-11-02 12:35:00 +0100

    Update utils/search_letters.py
    Co-authored-by: Alexander Kidanov <a.kidanov@gmail.com>

--- utils/search_letters.py
+++ utils/search_letters.py
@@ -3,7 +3,7 @@ def search_by_letters(search_string, contacts):
     for name, record in contacts.items():
         umail = str(record.email).lower()
         uname = name.lower()
-        if umail.find(search_string) != -1 or uname.find(search_string) != -1:
+        if search_string in [unmail, uname]:
             phone_numbers = [phone.value for phone in record.phones]
             result += f"{name}: {', '.join(phone_numbers)} email: {record.email}\n"
     return result
