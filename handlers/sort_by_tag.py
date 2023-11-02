3a1053b327ab8fcac3704d35f411cbfc2b9c403c
saniola <a.kidanov@gmail.com>
2023-11-02 20:46:57 +0200

    fix sort

--- handlers/sort_by_tag.py
+++ handlers/sort_by_tag.py
@@ -1,13 +1,12 @@
 from decorators.input_error import input_error
-from models.adressbook import AddressBook
+from models.notes import Notes
 
 @input_error
-def add_birthday(args, contacts: AddressBook):
-    name, birthday = args
-
-    if name in contacts:
-        record = contacts[name]
-        record.add_birthday(birthday)
-        return f"Birthday added for {name}."
-    else:
-        raise KeyError
+def sort_by_tag(args, notes: Notes):
+    '''Waiting for tag'''
+    tag = args[0]
+    matching_notes = notes.sort_notes_by_tag(tag)
+    if matching_notes:
+        print("Notes sorted by tag:")
+        for note in matching_notes:
+            print(str(note))
