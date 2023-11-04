a961b6e76e4d71e588c2c59c43e5707d837db187
guzz <144153106+guzzgrey@users.noreply.github.com>
2023-11-04 22:00:29 +0100

    changes by team

--- utils/get_next_week_birthdays.py
+++ utils/get_next_week_birthdays.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from utils.get_today import get_today
 
-def get_next_week_birthdays(address_book, birthdays_per_week,days):
+
+def get_next_week_birthdays(address_book, birthdays_per_week, days):
     today = get_today()
     today_datetime = datetime(today.year, today.month, today.day)
     users = address_book.values()
@@ -15,8 +16,19 @@ def get_next_week_birthdays(address_book, birthdays_per_week,days):
             delta_days = (birthday_this_year - today_datetime).days
 
             if delta_days <= int(days):
-                birthday_list += f"{user.birthday}: {name} Залишилось до дня народження: {delta_days}\n"
-            
+                if delta_days < 0:
+                    if is_leap_year(today.year + 1):
+                        delta_days += 366
+                    else:
+                        delta_days += 365
 
-     
+                birthday_list += f"{user.birthday}: {name} Залишилось до дня народження: {delta_days}\n"
+
     return birthday_list
+
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
