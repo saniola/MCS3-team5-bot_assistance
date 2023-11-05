1316f4505e8ab9cfccbbce400e1a074a500f744e
guzz <144153106+guzzgrey@users.noreply.github.com>
2023-11-05 19:02:19 +0100

    changes by team (#24)
    add better birthdays function 
    
    ---------
    
    Co-authored-by: Serhii Dzhulai <sergeyjuly@gmail.com>

--- utils/commands_completer.py
+++ utils/commands_completer.py
@@ -3,8 +3,8 @@ from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.styles import Style
 
 commands = [
-    "hello","add-contact", "edit-phone", "show-contacts", "edit-name", "show-phone", "add-birthday"\
-    "edit-birthday", "show-birthday", "birthdays", "add-email", "edit-email", "search"\
+    "hello","add-contact", "edit-phone", "show-contacts", "edit-name", "show-phone", "add-birthday",\
+    "edit-birthday", "show-birthday", "birthdays", "add-email", "edit-email", "search",\
     "delete-contact", "add-note", "show-notes", "show-tags", "edit-note-title", "edit-note-text",\
     "edit-note-title", "edit-node-text", "add-tag-to-note", "delete-tag-to-note", "find-notes-by-title",\
     "find-notes-by-tags", "sort-by-tag", "delete-note-by-title", "help", "add-address", "delete-address",\
--- utils/get_next_week_birthdays.py
+++ utils/get_next_week_birthdays.py
@@ -1,22 +1,46 @@
+from collections import UserDict
 from datetime import datetime
-from utils.get_today import get_today
 
-def get_next_week_birthdays(address_book, birthdays_per_week,days):
-    today = get_today()
-    today_datetime = datetime(today.year, today.month, today.day)
+
+def get_next_birthdays(address_book, days):
+    today_datetime = datetime.today()
     users = address_book.values()
-
-    birthday_list = "\n"
+    leap_date = datetime(3000, 3, 1)
+    result = UserDict()
     for user in users:
         if user.birthday:
-            name = user.name.value
             birthday = datetime.strptime(user.birthday.value, '%d.%m.%Y')
-            birthday_this_year = birthday.replace(year=today.year)
+            birthday_this_year = birthday.replace(year=today_datetime.year)
             delta_days = (birthday_this_year - today_datetime).days
 
+            if delta_days < 0:
+                if is_leap_year(today_datetime.year + 1) \
+                    and birthday.day >= leap_date.day \
+                    and birthday.month >=leap_date.month:
+                    delta_days += 366
+                else:
+                    delta_days += 365
             if delta_days <= int(days):
-                birthday_list += f"{user.birthday}: {name} Залишилось до дня народження: {delta_days}\n"
-            
+                if result.get(delta_days):
+                    result[delta_days] = result[delta_days] + f", \'{user.name.value}\'"
+                else:
+                    result[delta_days] = f"\'{user.name.value}\'"
 
-     
-    return birthday_list
+    return print_birdays(result)
+
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+def print_birdays(birdayes: UserDict):
+    res = ''
+    for k, val in sorted(birdayes.data.items()):
+        if k == 1:
+            res += f"Birthday in {k+1} day: {val}\n"
+        else:
+            res += f"Birthday in {k+1} days: {val}\n"
+
+    return res.strip('\n')
